a023cc57cf787067787e9dcb00a2acdc2b9004db
jing <jingshenglong@chinamcloud.com>
2018-04-09 15:54:35 +0800

    commit

--- git_push.sh
+++ git_push.sh
@@ -2,11 +2,13 @@
 echo "正在添加文件..."
 git add .
 echo -n "正在提交备注..."
-if [[  $1=' ' ]];then  
-   msg='commit ';  
-else  
-   msg=$1;  
-fi 
+remarks=$1
+if [ ! $remarks ] 
+then
+   msg="commit ";  
+else
+   msg=$remarks;  
+fi
 git commit -m "$msg"
 echo "正在开始提交代码..."
 git push github master
