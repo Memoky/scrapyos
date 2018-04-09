e373367efdf209e46b8ad39fd0cb0672cbc8bbfa
jing <jingshenglong@chinamcloud.com>
2018-04-09 18:17:26 +0800

    commit

--- scrapyos/items.py
+++ scrapyos/items.py
@@ -12,3 +12,10 @@ class ScrapyosItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class DoubanMovieItem(scrapy.Item):
+    ranking = scrapy.Field()
+    movie_name = scrapy.Field()
+    score = scrapy.Field()
+    score_num = scrapy.Field()
